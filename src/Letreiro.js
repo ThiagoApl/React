5af2c8032f9414d415b0ad671c759cd2dccaa9a4
Thiago Augustus Palacio <162067956+ThiagoApl@users.noreply.github.com>
2024-04-30 20:50:58 -0300

    Add files via upload

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,9 +1,17 @@
-function Letreiro () {
-  return (
-    <>
-     <h1>Conheça a Fatec</h1> 
-    </>
-  );
+import React, { useState, useEffect } from 'react';
+const TypingText = () => {
+  const text = ' Conheça a Fatec ';
+  const [index, steIndex] = useState(0);
+
+  useEffect(() => {
+    const timeoutld = setTimeout(()=> {
+      steIndex((prevIndex) =>(prevIndex + 1)% text.length);
+    },400); //Ajuste a velocidade
+
+    return ()=> clearTimeout(timeoutld);
+  },[index]);
+
+  return <h1>{text.slice(0, index)}</h1>;
 }
 
-export default Letreiro
+export default TypingText;
